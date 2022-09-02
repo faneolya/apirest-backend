c891aae7af975f09e9a59a90ffaf2148336b3eae
Faneolia Bissangoye <104846096+faneolya@users.noreply.github.com>
2022-09-02 09:32:17 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
+/*intégration base de donnée*/ 
+
 var express = require("express")
 var mysql = require("mysql")
 var app = express()
 
+/* analyse les requêtes de type de contenu - application/json*/
+
 app.use(express.json())
+
+/*connection*/
 
 const con = mysql.createConnection({
     host: 'localhost',
@@ -26,6 +32,7 @@ app.get('/', (req, res)=>{
     res.send('Hello');
 })
 
+/*récuperer une chaussure*/
 
 app.get('/api/get', (req, res)=>{
     
@@ -46,6 +53,7 @@ app.get('/api/get/:id', (req, res)=>{
     })
 })
 
+/*envoyer requêtes chaussures*/
 
 app.post('/api/chaussures/post', (req, res)=>{
     const id_marque = req.body.id_marque;
@@ -53,7 +61,8 @@ app.post('/api/chaussures/post', (req, res)=>{
     const taille = req.body.taille;
     const prix = req.body.prix;
 
-    
+/*ajouter une chaussure*/
+
     con.query('INSERT INTO chaussures VALUES(NULL,?,?,?,?)',[id_marque,couleur,taille,prix],(err,result)=>{
         if(err)
     {
@@ -64,11 +73,14 @@ app.post('/api/chaussures/post', (req, res)=>{
     })
 })
 
+/*envoyer requêtes marque*/
+
 app.post('/api/marques/post', (req, res)=>{
     const nom_de_la_marque = req.body.nom_de_la_marque;
     const logo = req.body.logo;
 
-    
+/*ajouter une marque*/
+
     con.query('INSERT INTO marques VALUES(NULL,?,?)',[nom_de_la_marque,logo],(err,result)=>{
         if(err)
     {
@@ -79,6 +91,7 @@ app.post('/api/marques/post', (req, res)=>{
     })
 })
 
+/*définit le port, écoute les requêtes*/
 
 app.listen(3003, (err)=>{
     if(err)
